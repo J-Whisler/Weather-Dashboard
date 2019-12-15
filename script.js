29f0173974001a94859e175cfed8aa9022915e3e
Jacob Whisler <jwhisler521@gmail.com>
2019-12-15 12:05:48 -0500

    added search bar and styled it, brought in a couple of elements from html to js

---
+++ script.js
@@ -0,0 +1,8 @@
+a09b9a0de48c45b63a70454f0000db8e
+
+// makes city input disappear, and 5 day weather forecast appear
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search-Btn')
+
+
+
